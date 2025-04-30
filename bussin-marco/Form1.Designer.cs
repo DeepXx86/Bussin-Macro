9b4e0213f911fef3c711f4b26d055234cbba8908
Pascal <167556769+DeepXx86@users.noreply.github.com>
2025-04-30 21:51:06 +0700

    Add files via upload

---
+++ bussin-marco/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace bussin_marco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.app = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(49)))));
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(-4, 429);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(662, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(179)))), ((int)(((byte)(223)))));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(584, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 42);
+            this.button5.TabIndex = 4;
+            this.button5.Text = ">";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(262, 17);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "clean";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "wait";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "press";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // app
+            // 
+            this.app.AutoScroll = true;
+            this.app.Location = new System.Drawing.Point(13, 13);
+            this.app.Name = "app";
+            this.app.Size = new System.Drawing.Size(630, 410);
+            this.app.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(514, 17);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 32);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "loop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(655, 490);
+            this.Controls.Add(this.app);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bussin-Marco";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel app;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ bussin-marco/Form1.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace bussin_marco
+
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        const int MOUSEEVENTF_LEFTUP = 0x04;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        const int MOUSEEVENTF_RIGHTUP = 0x10;
+        [DllImport("user32.dll")]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        static extern short VkKeyScan(char ch);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        const int INPUT_KEYBOARD = 1;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+        const uint VK_KEYDOWN = 0x0000;
+        const uint MAPVK_VK_TO_VSC = 0x00;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct INPUT
+        {
+            public int type;
+            public InputUnion u;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct InputUnion
+        {
+            [FieldOffset(0)] public KEYBDINPUT ki;
+            [FieldOffset(0)] public MOUSEINPUT mi;
+            [FieldOffset(0)] public HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
+
+        void PressKey(Keys key, int holdDuration)
+        {
+            try
+            {
+                ushort virtualKeyCode = (ushort)key;
+                ushort scanCode = (ushort)MapVirtualKey((uint)key, MAPVK_VK_TO_VSC);
+
+                INPUT[] inputs = new INPUT[2];
+
+                inputs[0].type = INPUT_KEYBOARD;
+                inputs[0].u.ki.wVk = virtualKeyCode;
+                inputs[0].u.ki.wScan = scanCode;
+                inputs[0].u.ki.dwFlags = 0; 
+                inputs[0].u.ki.time = 0;
+                inputs[0].u.ki.dwExtraInfo = IntPtr.Zero;
+
+                SendInput(1, new INPUT[] { inputs[0] }, Marshal.SizeOf(typeof(INPUT)));
+
+                Thread.Sleep(holdDuration);
+
+                inputs[1].type = INPUT_KEYBOARD;
+                inputs[1].u.ki.wVk = virtualKeyCode;
+                inputs[1].u.ki.wScan = scanCode;
+                inputs[1].u.ki.dwFlags = KEYEVENTF_KEYUP;
+                inputs[1].u.ki.time = 0;
+                inputs[1].u.ki.dwExtraInfo = IntPtr.Zero;
+
+                SendInput(1, new INPUT[] { inputs[1] }, Marshal.SizeOf(typeof(INPUT)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error pressing key: {ex.Message}", "Key Press Error");
+            }
+        }
+
+        void PressSpecialKey(Keys key, int holdDuration)
+        {
+            try
+            {
+                ushort virtualKeyCode = (ushort)key;
+                ushort scanCode = (ushort)MapVirtualKey((uint)key, MAPVK_VK_TO_VSC);
+
+                bool isExtendedKey = (key == Keys.Up || key == Keys.Down || key == Keys.Left || key == Keys.Right ||
+                                     key == Keys.Home || key == Keys.End || key == Keys.PageUp || key == Keys.PageDown ||
+                                     key == Keys.Insert || key == Keys.Delete || key == Keys.NumLock);
+
+                INPUT[] inputs = new INPUT[2];
+
+                inputs[0].type = INPUT_KEYBOARD;
+                inputs[0].u.ki.wVk = virtualKeyCode;
+                inputs[0].u.ki.wScan = scanCode;
+                inputs[0].u.ki.dwFlags = isExtendedKey ? KEYEVENTF_EXTENDEDKEY : 0;
+                inputs[0].u.ki.time = 0;
+                inputs[0].u.ki.dwExtraInfo = IntPtr.Zero;
+
+                SendInput(1, new INPUT[] { inputs[0] }, Marshal.SizeOf(typeof(INPUT)));
+
+                Thread.Sleep(holdDuration);
+
+                inputs[1].type = INPUT_KEYBOARD;
+                inputs[1].u.ki.wVk = virtualKeyCode;
+                inputs[1].u.ki.wScan = scanCode;
+                inputs[1].u.ki.dwFlags = KEYEVENTF_KEYUP | (isExtendedKey ? KEYEVENTF_EXTENDEDKEY : 0);
+                inputs[1].u.ki.time = 0;
+                inputs[1].u.ki.dwExtraInfo = IntPtr.Zero;
+
+                SendInput(1, new INPUT[] { inputs[1] }, Marshal.SizeOf(typeof(INPUT)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error pressing special key: {ex.Message}", "Key Press Error");
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                if (!macroReady)
+                {
+                    MessageBox.Show("Click Start first before using F8.", "Macro Not Ready");
+                    return;
+                }
+
+                if (!macroRunning)
+                {
+                    macroRunning = true;
+                    RunMacro();
+                }
+                else
+                {
+                    macroRunning = false;
+                    macroToken?.Cancel();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private async void RunMacro()
+        {
+            macroToken = new CancellationTokenSource();
+            var token = macroToken.Token;
+
+            try
+            {
+                do
+                {
+                    foreach (Control block in app.Controls)
+                    {
+                        if (!macroRunning || token.IsCancellationRequested) return;
+
+                        if (block.Controls.Count == 0) continue;
+                        Label lbl = block.Controls[0] as Label;
+                        if (lbl == null) continue;
+
+                        int hold = 0;
+                        Keys key = Keys.None;
+
+                        if (lbl.Text.StartsWith("🖱️") || lbl.Text.StartsWith("⏱️"))
+                        {
+                            hold = (int)block.Tag;
+                        }
+                        else if (lbl.Text.StartsWith("⌨️"))
+                        {
+                            var tag = (Tuple<Keys, int>)block.Tag;
+                            key = tag.Item1;
+                            hold = tag.Item2;
+                        }
+
+                        if (lbl.Text.StartsWith("🖱️"))
+                        {
+                            Point pos = Cursor.Position;
+                            // If right-click, use negative hold value
+                            if (hold >= 0)
+                            {
+                                mouse_event(MOUSEEVENTF_LEFTDOWN, pos.X, pos.Y, 0, 0);
+                                await Task.Delay(hold, token);
+                                mouse_event(MOUSEEVENTF_LEFTUP, pos.X, pos.Y, 0, 0);
+                            }
+                            else
+                            {
+                                int delay = Math.Abs(hold);
+                                mouse_event(MOUSEEVENTF_RIGHTDOWN, pos.X, pos.Y, 0, 0);
+                                await Task.Delay(delay, token);
+                                mouse_event(MOUSEEVENTF_RIGHTUP, pos.X, pos.Y, 0, 0);
+                            }
+                        }
+
+                        else if (lbl.Text.StartsWith("⌨️"))
+                        {
+                            
+                            if (key == Keys.Up || key == Keys.Down || key == Keys.Left || key == Keys.Right ||
+                                key == Keys.Home || key == Keys.End || key == Keys.PageUp || key == Keys.PageDown ||
+                                key == Keys.Insert || key == Keys.Delete || key == Keys.NumLock)
+                            {
+                                PressSpecialKey(key, hold);
+                            }
+                            else
+                            {
+                                PressKey(key, hold);
+                            }
+                        }
+                        else if (lbl.Text.StartsWith("⏱️"))
+                        {
+                            await Task.Delay(hold, token);
+                        }
+                    }
+                } while (isLoop && macroRunning && !token.IsCancellationRequested);
+            }
+            catch (TaskCanceledException)
+            {
+                
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_ID = 1;
+        private const int MOD_NONE = 0x0000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        public static void Curve(Control ctrl, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            Rectangle bounds = ctrl.ClientRectangle;
+            path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
+            path.AddArc(bounds.Right - radius, bounds.Y, radius, radius, 270, 90);
+            path.AddArc(bounds.Right - radius, bounds.Bottom - radius, radius, radius, 0, 90);
+            path.AddArc(bounds.X, bounds.Bottom - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+            ctrl.Region = new Region(path);
+        }
+        int blockY = 0; 
+        int blockHeight = 50;
+        int space = 10;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Panel block = new Panel();
+            block.Width = app.Width - 10;
+            block.Height = blockHeight;
+            block.Location = new Point(0, blockY);
+            block.BackColor = Color.FromArgb(223, 208, 184);
+            Curve(block, 15);
+
+            Label lbl = new Label();
+            lbl.Text = $"🖱️ Click (Hold: 0ms)";
+            lbl.Location = new Point(10, 15);
+            lbl.AutoSize = true;
+            lbl.Font = new Font("Segoe UI Emoji", 10);
+
+            block.Tag = 0;
+
+            block.Click += (s, evt) =>
+            {
+                int oldHold = (int)block.Tag;
+                string input = Microsoft.VisualBasic.Interaction.InputBox("Set Hold (ms)", "Hold Time", oldHold.ToString());
+                if (int.TryParse(input, out int newHold))
+                {
+                    block.Tag = newHold;
+                    lbl.Text = $"🖱️ Click (Hold: {newHold}ms)";
+                }
+            };
+
+            block.Controls.Add(lbl);
+            app.Controls.Add(block);
+            blockY += blockHeight + space;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Panel block = new Panel();
+            block.Width = app.Width - 10;
+            block.Height = blockHeight;
+            block.Location = new Point(0, blockY);
+            block.BackColor = Color.FromArgb(246, 222, 216);
+            Curve(block, 15);
+
+            Label lbl = new Label();
+            lbl.Text = $"⌨️ Press (Key: A, Hold: 0ms)";
+            lbl.Location = new Point(10, 15);
+            lbl.AutoSize = true;
+            lbl.Font = new Font("Segoe UI Emoji", 10);
+
+            block.Tag = Tuple.Create(Keys.A, 0);
+
+            block.Click += (s, evt) =>
+            {
+                var old = (Tuple<Keys, int>)block.Tag;
+
+                string keyInput = Interaction.InputBox("Enter key (like A, B, Enter):", "Key To Press", old.Item1.ToString());
+                string holdInput = Interaction.InputBox("Set Hold (ms)", "Hold Time", old.Item2.ToString());
+
+                if (Enum.TryParse(keyInput, true, out Keys chosenKey) && int.TryParse(holdInput, out int newHold))
+                {
+                    block.Tag = Tuple.Create(chosenKey, newHold);
+                    lbl.Text = $"⌨️ Press (Key: {chosenKey}, Hold: {newHold}ms)";
+                }
+                else
+                {
+                    MessageBox.Show("Invalid key or hold value.");
+                }
+            };
+
+            block.Controls.Add(lbl);
+            app.Controls.Add(block);
+            blockY += blockHeight + space;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Panel block = new Panel();
+            block.Width = app.Width - 10;
+            block.Height = blockHeight;
+            block.Location = new Point(0, blockY);
+            block.BackColor = Color.FromArgb(202, 224, 188);
+            Curve(block, 15);
+
+            Label lbl = new Label();
+            lbl.Text = $"⏱️ Wait: 0ms";
+            lbl.Location = new Point(10, 15);
+            lbl.AutoSize = true;
+            lbl.Font = new Font("Segoe UI Emoji", 10);
+
+            block.Tag = 0;
+
+            block.Click += (s, evt) =>
+            {
+                int oldHold = (int)block.Tag;
+                string input = Microsoft.VisualBasic.Interaction.InputBox("Set Hold (ms)", "Hold Time", oldHold.ToString());
+                if (int.TryParse(input, out int newHold))
+                {
+                    block.Tag = newHold;
+                    lbl.Text = $"⏱️ Wait: {newHold}ms";
+                }
+            };
+
+            block.Controls.Add(lbl);
+            app.Controls.Add(block);
+            blockY += blockHeight + space;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Are you wanna clear all blocks, fr?", "Confirm Clear", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                app.Controls.Clear();
+                blockY = 0;
+            }
+        }
+        bool run = false;
+        bool macroReady = false;
+        bool macroRunning = false;
+        CancellationTokenSource macroToken;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!macroReady)
+            {
+
+                button5.Text = "x";
+                button5.BackColor = Color.FromArgb(255, 130, 130);
+                button5.ForeColor = Color.White;
+                macroReady = true;
+                RegisterHotKey(this.Handle, HOTKEY_ID, MOD_NONE, (int)Keys.F8);
+            }
+            else
+            {
+                button5.Text = ">";
+                button5.BackColor = Color.FromArgb(159, 179, 223);
+                button5.ForeColor = Color.White;
+                macroReady = false;
+                macroRunning = false;
+                macroToken?.Cancel();
+                UnregisterHotKey(this.Handle, HOTKEY_ID);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+
+        bool isLoop = true;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            isLoop = !isLoop; 
+
+            if (isLoop)
+            {
+                button6.Text = "loop";
+                button6.BackColor = Color.White;
+            }
+            else
+            {
+                button6.Text = "one time";
+                button6.BackColor = Color.White;
+            }
+
+        }
+    }
+}
